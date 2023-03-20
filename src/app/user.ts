5758b86ff0252a95810d6a7b8e5e8d8ca3ef0d90
Jakub Kruliczak <jakubkruliczak@gmail.com>
2023-03-20 16:04:38 +0100

    raw code

---
+++ src/app/user.ts
@@ -0,0 +1,651 @@
+export interface User {
+  id: number;
+  avatar: string;
+  username: string;
+  email: string;
+}
+export const listOfUsers: User[] = [
+  {
+    id: 1,
+    avatar:
+      'https://robohash.org/errorrationerecusandae.png?size=125x125&set=set1',
+    username: 'lmaccoll0',
+    email: 'rbridell0@bloomberg.com',
+  },
+  {
+    id: 2,
+    avatar: 'https://robohash.org/isteevenietea.png?size=120x120&set=set1',
+    username: 'pellams1',
+    email: 'gcheasman1@amazon.co.uk',
+  },
+  {
+    id: 3,
+    avatar:
+      'https://robohash.org/debitismollitiaducimus.png?size=120x120&set=set1',
+    username: 'hsurmeyers2',
+    email: 'alemarchant2@livejournal.com',
+  },
+  {
+    id: 4,
+    avatar: 'https://robohash.org/voluptasillumin.png?size=120x120&set=set1',
+    username: 'xkittow3',
+    email: 'bmatterson3@biglobe.ne.jp',
+  },
+  {
+    id: 5,
+    avatar:
+      'https://robohash.org/earummagnilaboriosam.png?size=120x120&set=set1',
+    username: 'ggroomebridge4',
+    email: 'bdallimore4@wikipedia.org',
+  },
+  {
+    id: 6,
+    avatar: 'https://robohash.org/magnimodiaut.png?size=120x120&set=set1',
+    username: 'tsheldon5',
+    email: 'randreoletti5@nationalgeographic.com',
+  },
+  {
+    id: 7,
+    avatar:
+      'https://robohash.org/doloremnecessitatibustempora.png?size=120x120&set=set1',
+    username: 'acornils6',
+    email: 'rgrcic6@sciencedirect.com',
+  },
+  {
+    id: 8,
+    avatar: 'https://robohash.org/nemodoloremvitae.png?size=120x120&set=set1',
+    username: 'dbenda7',
+    email: 'tdutch7@prweb.com',
+  },
+  {
+    id: 9,
+    avatar: 'https://robohash.org/esteterror.png?size=120x120&set=set1',
+    username: 'abradfield8',
+    email: 'htuxill8@wikipedia.org',
+  },
+  {
+    id: 10,
+    avatar:
+      'https://robohash.org/cumpraesentiummollitia.png?size=120x120&set=set1',
+    username: 'opavluk9',
+    email: 'dcurlis9@paypal.com',
+  },
+  {
+    id: 11,
+    avatar: 'https://robohash.org/ipsaoccaecatiet.png?size=120x120&set=set1',
+    username: 'mguittona',
+    email: 'mbeynea@economist.com',
+  },
+  {
+    id: 12,
+    avatar: 'https://robohash.org/minusautaut.png?size=120x120&set=set1',
+    username: 'mburnesb',
+    email: 'dranceb@biglobe.ne.jp',
+  },
+  {
+    id: 13,
+    avatar:
+      'https://robohash.org/aliquamofficiisculpa.png?size=120x120&set=set1',
+    username: 'glivardc',
+    email: 'btotterdellc@patch.com',
+  },
+  {
+    id: 14,
+    avatar: 'https://robohash.org/illumquibusdamsed.png?size=120x120&set=set1',
+    username: 'dbennsd',
+    email: 'lbended@plala.or.jp',
+  },
+  {
+    id: 15,
+    avatar: 'https://robohash.org/quisquamquiid.png?size=120x120&set=set1',
+    username: 'edemogeote',
+    email: 'cdursleye@spotify.com',
+  },
+  {
+    id: 16,
+    avatar:
+      'https://robohash.org/ipsamexpeditaimpedit.png?size=120x120&set=set1',
+    username: 'dconyersf',
+    email: 'ntycef@hao123.com',
+  },
+  {
+    id: 17,
+    avatar: 'https://robohash.org/autimpeditut.png?size=120x120&set=set1',
+    username: 'oastyg',
+    email: 'cdegouyg@miibeian.gov.cn',
+  },
+  {
+    id: 18,
+    avatar:
+      'https://robohash.org/numquameaqueadipisci.png?size=120x120&set=set1',
+    username: 'dthreadgouldh',
+    email: 'tklossh@mashable.com',
+  },
+  {
+    id: 19,
+    avatar: 'https://robohash.org/facilisevenietquia.png?size=120x120&set=set1',
+    username: 'gcrowteri',
+    email: 'etemplemani@t-online.de',
+  },
+  {
+    id: 20,
+    avatar:
+      'https://robohash.org/temporeexplicabounde.png?size=120x120&set=set1',
+    username: 'cgarfordj',
+    email: 'hhazelj@imdb.com',
+  },
+  {
+    id: 21,
+    avatar: 'https://robohash.org/velitnihilbeatae.png?size=120x120&set=set1',
+    username: 'ledwardsonk',
+    email: 'wpicklek@sourceforge.net',
+  },
+  {
+    id: 22,
+    avatar: 'https://robohash.org/saepedelenitineque.png?size=120x120&set=set1',
+    username: 'bwickenl',
+    email: 'odemeltl@scribd.com',
+  },
+  {
+    id: 23,
+    avatar:
+      'https://robohash.org/consecteturquismolestiae.png?size=120x120&set=set1',
+    username: 'smenghim',
+    email: 'drutterm@theatlantic.com',
+  },
+  {
+    id: 24,
+    avatar:
+      'https://robohash.org/temporibuserrordolore.png?size=120x120&set=set1',
+    username: 'kcoppinsn',
+    email: 'dliebn@theguardian.com',
+  },
+  {
+    id: 25,
+    avatar:
+      'https://robohash.org/omnisvelitrepellendus.png?size=120x120&set=set1',
+    username: 'yweylando',
+    email: 'tlettsomo@pen.io',
+  },
+  {
+    id: 26,
+    avatar: 'https://robohash.org/voluptaslaborumest.png?size=120x120&set=set1',
+    username: 'hvecardp',
+    email: 'cyeldenp@wix.com',
+  },
+  {
+    id: 27,
+    avatar: 'https://robohash.org/quiaquasiomnis.png?size=120x120&set=set1',
+    username: 'psquirrelq',
+    email: 'ewhybrowq@soup.io',
+  },
+  {
+    id: 28,
+    avatar: 'https://robohash.org/eummagnamqui.png?size=120x120&set=set1',
+    username: 'baddessr',
+    email: 'jarendsr@theatlantic.com',
+  },
+  {
+    id: 29,
+    avatar: 'https://robohash.org/distinctioideos.png?size=120x120&set=set1',
+    username: 'tleatons',
+    email: 'ehallbords@nyu.edu',
+  },
+  {
+    id: 30,
+    avatar:
+      'https://robohash.org/voluptatibusquiconsequatur.png?size=120x120&set=set1',
+    username: 'icuseckt',
+    email: 'bcarnegiet@disqus.com',
+  },
+  {
+    id: 31,
+    avatar: 'https://robohash.org/remminusmagnam.png?size=120x120&set=set1',
+    username: 'chambletonu',
+    email: 'gocahsedyu@nih.gov',
+  },
+  {
+    id: 32,
+    avatar: 'https://robohash.org/accusantiumfugahic.png?size=120x120&set=set1',
+    username: 'gconaboyv',
+    email: 'vshoutv@paypal.com',
+  },
+  {
+    id: 33,
+    avatar: 'https://robohash.org/sequiquasinisi.png?size=120x120&set=set1',
+    username: 'fwoolpertw',
+    email: 'egiacomuzziw@dell.com',
+  },
+  {
+    id: 34,
+    avatar:
+      'https://robohash.org/consequunturmaioresexercitationem.png?size=120x120&set=set1',
+    username: 'jyushachkovx',
+    email: 'jivex@accuweather.com',
+  },
+  {
+    id: 35,
+    avatar: 'https://robohash.org/dictaeaeos.png?size=120x120&set=set1',
+    username: 'acopelloy',
+    email: 'jmarlandy@dyndns.org',
+  },
+  {
+    id: 36,
+    avatar:
+      'https://robohash.org/rerumaliquidexplicabo.png?size=120x120&set=set1',
+    username: 'abumfordz',
+    email: 'lrydzynskiz@com.com',
+  },
+  {
+    id: 37,
+    avatar:
+      'https://robohash.org/atconsequaturveritatis.png?size=120x120&set=set1',
+    username: 'mjahnel10',
+    email: 'ggherarducci10@house.gov',
+  },
+  {
+    id: 38,
+    avatar: 'https://robohash.org/aliquametquo.png?size=120x120&set=set1',
+    username: 'jbingley11',
+    email: 'lbramford11@aol.com',
+  },
+  {
+    id: 39,
+    avatar:
+      'https://robohash.org/facerererumsapiente.png?size=120x120&set=set1',
+    username: 'fpien12',
+    email: 'efahy12@shutterfly.com',
+  },
+  {
+    id: 40,
+    avatar:
+      'https://robohash.org/accusantiumitaquesunt.png?size=120x120&set=set1',
+    username: 'myouson13',
+    email: 'cromei13@java.com',
+  },
+  {
+    id: 41,
+    avatar:
+      'https://robohash.org/perferendisrepudiandaedebitis.png?size=120x120&set=set1',
+    username: 'spolo14',
+    email: 'cworsalls14@msn.com',
+  },
+  {
+    id: 42,
+    avatar:
+      'https://robohash.org/asperioresdebitisnesciunt.png?size=120x120&set=set1',
+    username: 'murlin15',
+    email: 'abrightwell15@independent.co.uk',
+  },
+  {
+    id: 43,
+    avatar:
+      'https://robohash.org/doloribusnumquamlaudantium.png?size=120x120&set=set1',
+    username: 'cfrostdick16',
+    email: 'lmoohan16@studiopress.com',
+  },
+  {
+    id: 44,
+    avatar:
+      'https://robohash.org/rerumdelenitialiquid.png?size=120x120&set=set1',
+    username: 'mdeye17',
+    email: 'mcassely17@guardian.co.uk',
+  },
+  {
+    id: 45,
+    avatar: 'https://robohash.org/estitaquenumquam.png?size=120x120&set=set1',
+    username: 'lbadcock18',
+    email: 'hbuckland18@cdbaby.com',
+  },
+  {
+    id: 46,
+    avatar:
+      'https://robohash.org/perferendisatquetenetur.png?size=120x120&set=set1',
+    username: 'ftremeer19',
+    email: 'bcopplestone19@globo.com',
+  },
+  {
+    id: 47,
+    avatar:
+      'https://robohash.org/voluptatesculpaminus.png?size=120x120&set=set1',
+    username: 'gdagwell1a',
+    email: 'acurness1a@spiegel.de',
+  },
+  {
+    id: 48,
+    avatar: 'https://robohash.org/nonrepudiandaeut.png?size=120x120&set=set1',
+    username: 'bbasilio1b',
+    email: 'fpettipher1b@china.com.cn',
+  },
+  {
+    id: 49,
+    avatar: 'https://robohash.org/impeditestnihil.png?size=120x120&set=set1',
+    username: 'ngarrick1c',
+    email: 'lfeldbaum1c@unesco.org',
+  },
+  {
+    id: 50,
+    avatar:
+      'https://robohash.org/oditrepellendusdeserunt.png?size=120x120&set=set1',
+    username: 'vsidney1d',
+    email: 'qhughf1d@google.co.uk',
+  },
+  {
+    id: 51,
+    avatar: 'https://robohash.org/ututexplicabo.png?size=120x120&set=set1',
+    username: 'rzecchini1e',
+    email: 'nlowre1e@bravesites.com',
+  },
+  {
+    id: 52,
+    avatar: 'https://robohash.org/utaliasvitae.png?size=120x120&set=set1',
+    username: 'pradmer1f',
+    email: 'triby1f@uiuc.edu',
+  },
+  {
+    id: 53,
+    avatar: 'https://robohash.org/utmagnirecusandae.png?size=120x120&set=set1',
+    username: 'nmeineck1g',
+    email: 'rhawket1g@360.cn',
+  },
+  {
+    id: 54,
+    avatar: 'https://robohash.org/teneturquiacum.png?size=120x120&set=set1',
+    username: 'cbree1h',
+    email: 'lryle1h@ask.com',
+  },
+  {
+    id: 55,
+    avatar: 'https://robohash.org/eumetquos.png?size=120x120&set=set1',
+    username: 'mfanstone1i',
+    email: 'hgreenrod1i@mashable.com',
+  },
+  {
+    id: 56,
+    avatar:
+      'https://robohash.org/voluptatumdictanobis.png?size=120x120&set=set1',
+    username: 'fsollis1j',
+    email: 'yjohann1j@eventbrite.com',
+  },
+  {
+    id: 57,
+    avatar: 'https://robohash.org/odiomaximequisquam.png?size=120x120&set=set1',
+    username: 'uhallworth1k',
+    email: 'tpiercy1k@admin.ch',
+  },
+  {
+    id: 58,
+    avatar: 'https://robohash.org/erroreumcommodi.png?size=120x120&set=set1',
+    username: 'mfeaster1l',
+    email: 'jrawles1l@uiuc.edu',
+  },
+  {
+    id: 59,
+    avatar:
+      'https://robohash.org/explicaboexpeditaofficiis.png?size=120x120&set=set1',
+    username: 'bstquenin1m',
+    email: 'dlanfer1m@etsy.com',
+  },
+  {
+    id: 60,
+    avatar: 'https://robohash.org/rerumetconsequatur.png?size=120x120&set=set1',
+    username: 'rfutty1n',
+    email: 'jroskelley1n@storify.com',
+  },
+  {
+    id: 61,
+    avatar: 'https://robohash.org/nequesedest.png?size=120x120&set=set1',
+    username: 'cfreda1o',
+    email: 'rfyldes1o@moonfruit.com',
+  },
+  {
+    id: 62,
+    avatar:
+      'https://robohash.org/velitdolorummaiores.png?size=120x120&set=set1',
+    username: 'krobertacci1p',
+    email: 'rtubby1p@dell.com',
+  },
+  {
+    id: 63,
+    avatar: 'https://robohash.org/eosquidolor.png?size=120x120&set=set1',
+    username: 'zferrucci1q',
+    email: 'siacopo1q@geocities.com',
+  },
+  {
+    id: 64,
+    avatar: 'https://robohash.org/aperiamexpeditahic.png?size=120x120&set=set1',
+    username: 'lcheesworth1r',
+    email: 'mtunnow1r@ibm.com',
+  },
+  {
+    id: 65,
+    avatar: 'https://robohash.org/ducimusmagnamtotam.png?size=120x120&set=set1',
+    username: 'cashworth1s',
+    email: 'acaughan1s@webeden.co.uk',
+  },
+  {
+    id: 66,
+    avatar: 'https://robohash.org/nostrumautofficiis.png?size=120x120&set=set1',
+    username: 'dhatherley1t',
+    email: 'abesant1t@smugmug.com',
+  },
+  {
+    id: 67,
+    avatar:
+      'https://robohash.org/dignissimosautdoloremque.png?size=120x120&set=set1',
+    username: 'jbrehaut1u',
+    email: 'rthomlinson1u@independent.co.uk',
+  },
+  {
+    id: 68,
+    avatar:
+      'https://robohash.org/deseruntrepudiandaenumquam.png?size=120x120&set=set1',
+    username: 'abarrasse1v',
+    email: 'amoar1v@state.gov',
+  },
+  {
+    id: 69,
+    avatar:
+      'https://robohash.org/nequevoluptatesperspiciatis.png?size=120x120&set=set1',
+    username: 'alinnane1w',
+    email: 'sheisham1w@paypal.com',
+  },
+  {
+    id: 70,
+    avatar: 'https://robohash.org/earumrerumdolorem.png?size=120x120&set=set1',
+    username: 'rclews1x',
+    email: 'sseeborne1x@4shared.com',
+  },
+  {
+    id: 71,
+    avatar:
+      'https://robohash.org/temporibusnihilmodi.png?size=120x120&set=set1',
+    username: 'ggofton1y',
+    email: 'klorraway1y@indiegogo.com',
+  },
+  {
+    id: 72,
+    avatar:
+      'https://robohash.org/repudiandaenisiblanditiis.png?size=120x120&set=set1',
+    username: 'zferreli1z',
+    email: 'dclaige1z@netlog.com',
+  },
+  {
+    id: 73,
+    avatar: 'https://robohash.org/nullaquiaiusto.png?size=120x120&set=set1',
+    username: 'dsherebrook20',
+    email: 'ybarehead20@sfgate.com',
+  },
+  {
+    id: 74,
+    avatar:
+      'https://robohash.org/nobistemporaaccusantium.png?size=120x120&set=set1',
+    username: 'mtaunton21',
+    email: 'jkennea21@tinypic.com',
+  },
+  {
+    id: 75,
+    avatar:
+      'https://robohash.org/minussimiliqueofficia.png?size=120x120&set=set1',
+    username: 'adrogan22',
+    email: 'bclemitt22@devhub.com',
+  },
+  {
+    id: 76,
+    avatar: 'https://robohash.org/cumquedoloresipsa.png?size=120x120&set=set1',
+    username: 'kfryatt23',
+    email: 'gchitson23@phpbb.com',
+  },
+  {
+    id: 77,
+    avatar:
+      'https://robohash.org/molestiaedelenitiut.png?size=120x120&set=set1',
+    username: 'kchadband24',
+    email: 'lpoker24@cpanel.net',
+  },
+  {
+    id: 78,
+    avatar:
+      'https://robohash.org/inventoreaspernaturut.png?size=120x120&set=set1',
+    username: 'mboldt25',
+    email: 'iscripture25@tripod.com',
+  },
+  {
+    id: 79,
+    avatar:
+      'https://robohash.org/reprehenderitsedaliquid.png?size=120x120&set=set1',
+    username: 'sogilvie26',
+    email: 'nreims26@ow.ly',
+  },
+  {
+    id: 80,
+    avatar: 'https://robohash.org/eumvoluptatemqui.png?size=120x120&set=set1',
+    username: 'msidwell27',
+    email: 'ljoburn27@mozilla.com',
+  },
+  {
+    id: 81,
+    avatar: 'https://robohash.org/quietaut.png?size=120x120&set=set1',
+    username: 'dsnowling28',
+    email: 'gcowpland28@theatlantic.com',
+  },
+  {
+    id: 82,
+    avatar:
+      'https://robohash.org/quibusdameligendiaspernatur.png?size=120x120&set=set1',
+    username: 'wstovell29',
+    email: 'fcoley29@usgs.gov',
+  },
+  {
+    id: 83,
+    avatar: 'https://robohash.org/eosvelest.png?size=120x120&set=set1',
+    username: 'cgull2a',
+    email: 'coller2a@loc.gov',
+  },
+  {
+    id: 84,
+    avatar: 'https://robohash.org/iurelaudantiumsint.png?size=120x120&set=set1',
+    username: 'sscoggans2b',
+    email: 'fwaddilow2b@oakley.com',
+  },
+  {
+    id: 85,
+    avatar: 'https://robohash.org/etipsumut.png?size=120x120&set=set1',
+    username: 'kpignon2c',
+    email: 'mswatheridge2c@wunderground.com',
+  },
+  {
+    id: 86,
+    avatar: 'https://robohash.org/quodquivoluptatum.png?size=120x120&set=set1',
+    username: 'omateuszczyk2d',
+    email: 'lscannell2d@aboutads.info',
+  },
+  {
+    id: 87,
+    avatar: 'https://robohash.org/inestaspernatur.png?size=120x120&set=set1',
+    username: 'gaveline2e',
+    email: 'dguillerman2e@msn.com',
+  },
+  {
+    id: 88,
+    avatar: 'https://robohash.org/estnesciuntsit.png?size=120x120&set=set1',
+    username: 'nnorville2f',
+    email: 'jwhitfeld2f@github.com',
+  },
+  {
+    id: 89,
+    avatar: 'https://robohash.org/idiurequi.png?size=120x120&set=set1',
+    username: 'fpotebury2g',
+    email: 'delse2g@naver.com',
+  },
+  {
+    id: 90,
+    avatar: 'https://robohash.org/eaetmaxime.png?size=120x120&set=set1',
+    username: 'tgierhard2h',
+    email: 'scasacchia2h@alexa.com',
+  },
+  {
+    id: 91,
+    avatar: 'https://robohash.org/nobisatmolestias.png?size=120x120&set=set1',
+    username: 'ichaudrelle2i',
+    email: 'pswalteridge2i@biglobe.ne.jp',
+  },
+  {
+    id: 92,
+    avatar:
+      'https://robohash.org/recusandaeullamadipisci.png?size=120x120&set=set1',
+    username: 'rdeknevet2j',
+    email: 'jbransden2j@go.com',
+  },
+  {
+    id: 93,
+    avatar: 'https://robohash.org/ideaquesed.png?size=120x120&set=set1',
+    username: 'bsabathier2k',
+    email: 'aoneil2k@symantec.com',
+  },
+  {
+    id: 94,
+    avatar: 'https://robohash.org/vitaeestlaborum.png?size=120x120&set=set1',
+    username: 'rantonioni2l',
+    email: 'npolgreen2l@deliciousdays.com',
+  },
+  {
+    id: 95,
+    avatar:
+      'https://robohash.org/voluptatemdoloresquo.png?size=120x120&set=set1',
+    username: 'aellett2m',
+    email: 'gknewstubb2m@networksolutions.com',
+  },
+  {
+    id: 96,
+    avatar:
+      'https://robohash.org/accusantiumrationeeius.png?size=120x120&set=set1',
+    username: 'ariches2n',
+    email: 'mrelton2n@nydailynews.com',
+  },
+  {
+    id: 97,
+    avatar: 'https://robohash.org/autquidoloribus.png?size=120x120&set=set1',
+    username: 'jgarraway2o',
+    email: 'fgliddon2o@opera.com',
+  },
+  {
+    id: 98,
+    avatar:
+      'https://robohash.org/officiaplaceatdolorem.png?size=120x120&set=set1',
+    username: 'ktowler2p',
+    email: 'lfolds2p@uol.com.br',
+  },
+  {
+    id: 99,
+    avatar: 'https://robohash.org/sedfacerenulla.png?size=120x120&set=set1',
+    username: 'rwaring2q',
+    email: 'dmacane2q@google.fr',
+  },
+  {
+    id: 100,
+    avatar: 'https://robohash.org/excepturiipsaanimi.png?size=120x120&set=set1',
+    username: 'bluckhurst2r',
+    email: 'gzohrer2r@pcworld.com',
+  },
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { User, listOfUsers } from './../user';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent {
+  users: User[] = listOfUsers;
+}
